9846169041a401a61f545f1e3965bbb7e43a6fb4
Satyam Raj <satyambpl.2000@gmail.com>
2026-05-31 22:47:19 +0530

    Add comment for authentication purpose in authRoutes
    Added a comment to indicate the purpose of the router.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -3,7 +3,7 @@ import { registerUser, authUser, getMe } from '../controllers/authController.js'
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
-
+// for authentication...
 router.post('/register', registerUser);
 router.post('/login', authUser);
 router.get('/me', protect, getMe);
