b9aae14ebd874ea9ded7270eb434b17f8e8869f4
Satyam Raj <smritisoni@DODO-20.local>
2026-06-26 11:53:41 +0530

    new update in # url...

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 import { SocketProvider } from './context/SocketContext'
 import { ThemeProvider } from './context/ThemeContext'
 import { GlobalLoadingProvider } from './context/GlobalLoadingContext'
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <ThemeProvider>
         <GlobalLoadingProvider>
           <AuthProvider>
@@ -20,6 +20,6 @@ createRoot(document.getElementById('root')).render(
           </AuthProvider>
         </GlobalLoadingProvider>
       </ThemeProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
